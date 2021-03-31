ce6c60508d3dd134869ec59e0783b0e46c7db183
Jose Hernandez <josemanuelhdez2@gmail.com>
2021-03-31 10:25:03 +0100

    step 6: services

--- src/app/habit-list/habit-list.component.ts
+++ src/app/habit-list/habit-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { HabitService } from '../habit.service';
 
 @Component({
   selector: 'app-habit-list',
@@ -7,38 +9,23 @@ import { FormBuilder } from '@angular/forms';
     <h2>Habits</h2>
     <app-habit-form (addHabit)="onAddHabit($event)"></app-habit-form>
     <ul>
-      <app-habit-item *ngFor="let habit of habits" [habit]="habit"></app-habit-item>
+      <app-habit-item *ngFor="let habit of habits | async" [habit]="habit"></app-habit-item>
     </ul>
   `,
   styles: [
   ]
 })
 export class HabitListComponent implements OnInit {
-  habitForm;
-  habits = [
-    {
-      id: 1,
-      title: "Play games"
-    },
-    {
-      id: 2,
-      title: "Take names"
-    }
-  ]
+  habits: Observable<any>
 
-  constructor(private formBuilder: FormBuilder) { 
-    this.habitForm = this.formBuilder.group({
-      title: '',
-    })
-  }
+  constructor(private habitService: HabitService) {}
 
   onAddHabit(newHabit) {
-    const id = this.habits.length + 1;
-    newHabit.id = id;
-    this.habits.push(newHabit);
+    this.habitService.addHabit(newHabit);
   }
 
   ngOnInit(): void {
+    this.habits = this.habitService.getHabits();
   }
 
 }
--- src/app/habit.service.ts
+++ src/app/habit.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -16,4 +17,14 @@ export class HabitService {
   ]
 
   constructor() { }
+
+  getHabits() : Observable<any> {
+    return of(this.habits);
+  }
+
+  addHabit(newHabit) {
+    const id = this.habits.length + 1;
+    newHabit.id = id;
+    this.habits.push(newHabit);
+  }
 }
